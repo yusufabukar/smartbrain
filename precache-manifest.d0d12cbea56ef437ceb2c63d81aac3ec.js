6179717309670c7337b1fe52a26a88bff8e6e51a
yusufabukar <@yabukar64@gmail.com>
2020-05-25 18:44:46 +0100

    Updates

---
+++ precache-manifest.d0d12cbea56ef437ceb2c63d81aac3ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc5acdf9e11be3be97dc2b24b183d5d",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "416d83aed67c7c48b895",
+    "url": "/smartbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2e236aa77c4c50662f23",
+    "url": "/smartbrain/static/css/main.f125fea9.chunk.css"
+  },
+  {
+    "revision": "416d83aed67c7c48b895",
+    "url": "/smartbrain/static/js/2.5a981771.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smartbrain/static/js/2.5a981771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e236aa77c4c50662f23",
+    "url": "/smartbrain/static/js/main.0ce29e29.chunk.js"
+  },
+  {
+    "revision": "d5106abbe4fdad77a194",
+    "url": "/smartbrain/static/js/runtime-main.07838e49.js"
+  }
+]);
